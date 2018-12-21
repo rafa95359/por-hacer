47292160d8865f3837b547ab347e220002c776ab
Rafael <rafalopezlizana@gmail.com>
2018-12-20 19:57:01 -0500

    primer commit

---
+++ app.js
@@ -0,0 +1,44 @@
+const argv = require ('./config/yargs').argv;
+const color =require('colors');
+
+const porHacer = require ('./por-hacer/por-hacer');
+
+
+
+let comando = argv._[0];
+
+switch (comando){
+    case 'crear':
+    let tarea = porHacer.crear(argv.descripcion);
+    console.log(tarea);
+    break;
+
+    case 'listar':
+    let listado = porHacer.getListado();
+    for(let tarea of listado){
+        console.log('========Por Hacer====='.green);
+        console.log(tarea.descripcion);
+        console.log('Estado: ',tarea.completado);
+        console.log('======================'.green);
+        
+    }
+    //console.log('Mostrar todas las tareas  por hacer');
+    break;
+    
+    case 'actualizar':
+    let actualizado = porHacer.actualizar(argv.descripcion,argv.completado);
+    console.log(actualizado);
+    //console.log('Actualizar todas las tareas por hacer');
+    break;
+
+    case 'borrar':
+        let borrado = porHacer.borrar(argv.descripcion);
+        console.log(borrado);
+        break;
+        
+
+    default:
+    console.log('Comando no reconocido');
+}
+
+
